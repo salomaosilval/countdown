9a9942dc6c9630d3dd7fa2bb7e122e567990e856
Salomão Silva <salomaosilval@hotmail.com>
2022-12-27 20:50:55 -0400

    feat: Using the data generated by the hook

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,17 @@ import useCountdown from './hooks/useCountdown'
 import './App.css'
 
 function App() {
-  const x = useCountdown("Jan 1, 2023 00:00:00");
+  const [day, hour, minute, second] = useCountdown("Jan 1, 2023 00:00:00");
 
   return (
     <div className="App" style={{backgroundImage: `url(${NewYear})`}}>
       <div className="container">
         <Title title="Contagem regressiva para 2023"/>
         <div className="countdown-container">
-          <Counter title="Dias" number={2}/>
-          <Counter title="Horas" number={2}/>
-          <Counter title="Minutos" number={2}/>
-          <Counter title="Segundos" number={2}/>
+          <Counter title="Dias" number={day}/>
+          <Counter title="Horas" number={hour}/>
+          <Counter title="Minutos" number={minute}/>
+          <Counter title="Segundos" number={second}/>
         </div>
       </div>
     </div>
