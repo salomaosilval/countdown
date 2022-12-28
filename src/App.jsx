cc2e1aa1a4efa3127a359194872d86c689941dfe
Salomão Silva <salomaosilval@hotmail.com>
2022-12-27 20:33:49 -0400

    feat: standard structure of the complete project

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import Title from './components/Title'
+import Counter from './components/Counter'
+
+import NewYear from './assets/newyear.png'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App" style={{backgroundImage: `url(${NewYear})`}}>
+      <div className="container">
+        <Title title="Contagem regressiva para 2023"/>
+        <div className="countdown-container">
+          <Counter title="Dias" number={2}/>
+          <Counter title="Horas" number={2}/>
+          <Counter title="Minutos" number={2}/>
+          <Counter title="Segundos" number={2}/>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
